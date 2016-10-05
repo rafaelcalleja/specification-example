be6546624df0b1787651ceb7f0b9ccb290924425
rafaelcalleja <rafaelcalleja@gmail.com>
2016-10-05 17:07:03 +0200

    implementa abstract factory

---
+++ src/Example/Application/Service/EarliestEmployeesService2.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Example\Application\Service;
+
+use Example\Domain\Model\Employee\EmployeeRepository;
+use Example\Domain\Model\Employee\EmployeeSpecificationFactory;
+
+class EarliestEmployeesService
+{
+
+    /**
+     * @var EmployeeRepository
+     */
+    private $repository;
+
+    /**
+     * @var EmployeeSpecificationFactory
+     */
+    private $employeeSpecificationFactory;
+
+    public function __construct(EmployeeRepository $repository, EmployeeSpecificationFactory $employeeSpecificationFactory)
+    {
+
+        $this->repository = $repository;
+        $this->employeeSpecificationFactory = $employeeSpecificationFactory;
+    }
+
+    public function execute($request)
+    {
+        // ...
+
+        $employees = $this->repository->query(
+            $this->employeeSpecificationFactory->createEarliestEmployees(
+                new \DateTimeImmutable('-1 year')
+            )
+        );
+        
+        /**
+         *  Comentario:
+         *
+         * Desde el controlador sin usar el abstract factory pattern
+         *
+         * $repository = new SqlEmployeesService();
+         *
+         * $service = new EarliestEmployeesService($repository, new SqlFromEmployeeSpecification($date) ) // es un problemna que la especificaion tenga que recibir la fecha
+         *
+         *
+        **/
+
+        // ...
+    }
+}
